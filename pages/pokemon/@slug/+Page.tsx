a7cbd0d3795a129bd526c144a0274523ccb11f4c
Justine Bouin <112966252+alt-ju@users.noreply.github.com>
2024-12-08 12:50:04 +0100

    feat: display image pokemon

--- pages/pokemon/@slug/+Page.tsx
+++ pages/pokemon/@slug/+Page.tsx
@@ -1,33 +1,78 @@
-import { useData } from "vike-react/useData";
+import { useData } from "vike-react/useData"
 import { useState, useContext } from 'react'
-import type { Data } from "./+data";
-import { TeamContext } from "../../../context/TeamContext";
+import type { Data } from "./+data"
+import { TeamContext } from "../../../context/TeamContext"
 
 export default function PagePokemonUnique() {
     const data = useData<Data>()
     const [pokemon, setPokemon] = useState(data)
     const teamContext = useContext(TeamContext)
     const { addToTeam } = teamContext
+    const [ selectedImage, setSelectedImage ] = useState<string | null>(pokemon.sprites.shiny.male)
+
+    const handleImageSelection = (image: string) => {
+        setSelectedImage(image)
+    };
     
     return (
         <>
         <div className="container-details">
             <p>{ pokemon.name }</p>
-            { pokemon.types.map((type: any) => (
-                <p>{ type.name }</p>
-            ))}
-            { pokemon.stats.map((stat: any) => (
-                <div className="stats">
-                    <p>{ stat.name }</p>
-                    <p>{ stat.base_stat }</p>
-                </div>
-            ))}
+            <div className="button-container">
+                {pokemon.sprites.normal.male && (
+                    <button
+                        className="btn"
+                        onClick={() => handleImageSelection(pokemon.sprites.normal.male)}
+                    >
+                        Normal Male
+                    </button>
+                )}
+                {pokemon.sprites.normal.female && (
+                    <button
+                        className="btn"
+                        onClick={() => handleImageSelection(pokemon.sprites.normal.female)}
+                    >
+                        Normal Female
+                    </button>
+                )}
+                {pokemon.sprites.shiny.male && (
+                    <button
+                        className="btn"
+                        onClick={() => handleImageSelection(pokemon.sprites.shiny.male)}
+                    >
+                        Shiny Male
+                    </button>
+                )}
+                {pokemon.sprites.shiny.female && (
+                    <button
+                        className="btn"
+                        onClick={() => handleImageSelection(pokemon.sprites.shiny.female)}
+                    >
+                        Shiny Female
+                    </button>
+                )}
+            </div>
+            <img src={selectedImage || ""} alt={selectedImage ? `Image de ${pokemon.name}` : ""} />
+            <p>Types:</p>
+            <div className="types-container">
+                { pokemon.types.map((type: any) => (
+                    <div className="types">
+                        <p>{ type.name }</p>
+                    </div> 
+                ))}
+            </div>
+            <div className="stats-container">
+                { pokemon.stats.map((stat: any) => (
+                    <div className="stats">
+                        <p>{ stat.name }</p>
+                        <p>{ stat.base_stat }</p>
+                    </div>
+                ))}
+            </div>
             <div>
                 <button onClick={() => addToTeam(pokemon)}>Ajouter à l'équipe</button>
             </div>
         </div>
-            
-            
         </>
     )
 }
--- pages/pokemon/teams/Page.telefunc.ts
+++ pages/pokemon/teams/Page.telefunc.ts
@@ -1,8 +1,9 @@
 import { useContext } from "react"
 import { TeamContext } from "../../../context/TeamContext"
 
+const token = 'advanced-pokedex-api-key-9sd1u98cvg4t98yi'
+
 export const onGetPokemons = async (...pokemonSlugs: string[]) => {
-    const token = 'advanced-pokedex-api-key-9sd1u98cvg4t98yi'
 
     const pokemons = await Promise.all(pokemonSlugs.map(async (poke) => {
         const reponse = await fetch(`https://pokedex.coda.memento-dev.fr/pokemon/${poke.slug}`, {
